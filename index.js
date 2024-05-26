d50a587ed684a3a788c4d229e6db769d5ae0a5a6
kosausrk <kosausrk@gmail.com>
2024-05-26 13:01:04 -0400

    added code

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 //Leet Code: Best Time to Buy and Sell Stock with Cooldown
 
+let prices = [1, 2, 2, 0, 3]; //Sample set 
+
+let maxProfit = function() {
+    let memo = new Map(); // Key: `${i},${buying}`, Value: maxProfit **we use template literals to make code cleaner 
+
+    function calculateMaxProfit(i, buying) {
+        // if list is empty theres no profit (return 0)
+        if (i >= prices.length) {
+            return 0;
+        }
+
+        //shorthand so we dont have to nest the value pair each time in our code 
+        let key = `${i},${buying}`;
+        // if val exists return respective profit 
+        if (memo.has(key)) {
+            return memo.get(key);
+        }
 
 
-let myArr = [1,2,3] //-3
-let profit = 0; 
-for (let i=0; i < myArr.length;i++){
-    if (myArr[i]){
-        profit = profit - myArr[i]
+        //recurssion begins 
+        let result; //based on decision tree for all possibilities 
+        if (buying) {
+            // Option 1: Buy the stock on day i
+            let buy = calculateMaxProfit(i + 1, false) - prices[i]; 
+            // Option 2: Do nothing (cooldown)
+            let cooldown = calculateMaxProfit(i + 1, true);
+            // Choose the option with the maximum profit
+            result = Math.max(buy, cooldown);
+        } else {
+            // Option 1: Sell the stock on day i
+            let sell = calculateMaxProfit(i + 1, true) + prices[i];
+            // Option 2: Do nothing (cooldown)
+            let cooldown = calculateMaxProfit(i + 1, false);
+            // Choose the option with the maximum profit
+            result = Math.max(sell, cooldown);
+        }
+
+        // store (index, buying), profit 
+        memo.set(key, result);
+        return result;
     }
-    console.log("number: " + myArr[i])
-}
 
-console.log(profit)
+    // Start the recursion from day 0 with the ability to buy
+    return calculateMaxProfit(0, true);
+};
+
+console.log(maxProfit()); 
