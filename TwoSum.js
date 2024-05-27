4c08ca9d48fa69b9e38dbcf5d81e4871eb2873a3
kosausrk <kosausrk@gmail.com>
2024-05-27 10:52:09 -0400

    twosum leetcode

--- TwoSum.js
+++ TwoSum.js
@@ -1,1 +1,16 @@
-console.log("Hello World")
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+let twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length;i++){
+        for (let j = i+1; j < nums.length;j++){
+            let combine = nums[i] + nums[j]
+            if (combine === target){
+                return new Array(i, j)
+            }
+        }
+    }  
+};
